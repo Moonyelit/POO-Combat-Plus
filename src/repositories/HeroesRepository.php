a2a088e415bf1b5a251710ee2138a00fabfa633d
moonye@live.fr <moonye@live.fr>
2025-01-16 17:02:54 +0100

    ADD : created

--- src/repositories/HeroesRepository.php
+++ src/repositories/HeroesRepository.php
@@ -7,31 +7,46 @@ final class HeroesRepository extends AbstractRepository
         parent::__construct();
     }
 
-public function FindAll(): array
-{
-    $query = "SELECT * FROM hero";
-    $stmt = $this->pdo->query($query);
-    $heroDatas = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $heroes = [];
+    public function FindAll(): array
+    {
+        $query = "SELECT * FROM hero";
+        $stmt = $this->pdo->query($query);
+        $heroDatas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $heroes = [];
 
-    foreach ($heroDatas as $heroData) {
-        $heroes[] = HeroMapper::mapToObject($heroData);
+        foreach ($heroDatas as $heroData) {
+            $heroes[] = HeroMapper::mapToObject($heroData);
+        }
+
+        return $heroes;
     }
 
-    return $heroes;
-}
+
+
+    public function FindOne(string $name): ?BaseHero
+    {
+        $query = "SELECT * FROM hero WHERE name = :name";
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute(['name' => $name]);
+        $heroData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+        if (!$heroData) {
+            return null;
+        }
+
+        return HeroMapper::mapToObject($heroData);
+    }
 
 
 
-public function FindOne(int $id): BaseHero
-{
-    $query = "SELECT * FROM hero WHERE name = :name";
-    $stmt = $this->pdo->prepare($query);
-    $stmt->execute(['id' => $id]);
-    $heroData = $stmt->fetch(PDO::FETCH_ASSOC);
+    public function saveHero(string $name): ?BaseHero
+    {
+        $stmt = $this->pdo->prepare('INSERT INTO hero (name) VALUES (:name)');
+        $stmt->execute(['name' => $name]);
 
-    return HeroMapper::mapToObject($heroData);
-}
+        $heroCreation = self::FindOne($name);
 
-
+        return $heroCreation;
+    }
 }
